e41c72c2e92faa85783b5874199db4992934d75c
BlueMingu블밍 <sc62070@gmail.com>
2022-09-26 06:40:12 +0000

    urls and views edits

--- djangoletsgo/djangoletsgo/settings.py
+++ djangoletsgo/djangoletsgo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1n4)(ywr$j$wk$cxju0ay!$t*if68btg^cx-+wkwms(f2!xnx7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
